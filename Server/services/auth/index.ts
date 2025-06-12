cde41b12d3a30aca1aec7c54db955a175b7529e6
Vajrap <vajra.p@pair-co.com>
2025-06-12 10:50:48 +0700

    init 2

--- Server/services/auth/index.ts
+++ Server/services/auth/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { serve } from "bun";
+import "dotenv/config";
+import { handleRequest } from "./routes/router";
+
+
+const PORT = parseInt(process.env.PORT || "3000");
+
+serve({
+  port: PORT,
+  fetch: handleRequest,
+});
